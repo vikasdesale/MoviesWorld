92cbc065cf0ee6370b21c1f636a4035bf4977b62
vikasdesale <vikas.desaleu@gmail.com>
2016-12-20 19:10:09 +0530

    3.1_added_details

---
+++ app/src/main/java/com/udacity/project2/popularmovies/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.udacity.project2.popularmovies;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+}
