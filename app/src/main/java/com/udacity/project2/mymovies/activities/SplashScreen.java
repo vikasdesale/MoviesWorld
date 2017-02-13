cb5af734efd6e726720ad0333f86a7adece4dcc5
vikasdesale <vikas.desaleu@gmail.com>
2017-02-13 21:13:58 +0530

    lauched_main_in_splash_activity

--- app/src/main/java/com/udacity/project2/mymovies/activities/SplashScreen.java
+++ app/src/main/java/com/udacity/project2/mymovies/activities/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.udacity.project2.mymovies.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.udacity.project2.mymovies.R;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
