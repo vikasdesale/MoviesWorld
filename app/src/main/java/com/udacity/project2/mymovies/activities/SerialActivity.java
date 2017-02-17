58cb6687df24aa8375c3a1bb4aa3baedf4afad66
vikasdesale <vikas.desaleu@gmail.com>
2017-02-17 21:52:22 +0530

    tv_serial_activity

--- app/src/main/java/com/udacity/project2/mymovies/activities/SerialActivity.java
+++ app/src/main/java/com/udacity/project2/mymovies/activities/SerialActivity.java
@@ -1,8 +1,26 @@
 package com.udacity.project2.mymovies.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by Dell on 2/15/2017.
  */
 
-public class SerialActivity {
+public class SerialActivity extends ActionBarActivity{
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
